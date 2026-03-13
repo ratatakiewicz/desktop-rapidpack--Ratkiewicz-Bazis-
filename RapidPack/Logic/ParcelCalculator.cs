f1cc801136e603b4fc4e88c9c03283b1cc2cb2f1
Mikołaj B-M <mikolaj@MacBook-Pro-Mikoaj.local>
2026-03-13 08:37:04 +0100

    Zadanie #6: Implementacja pełnego algorytmu wyceny i walidacji wagi.

--- RapidPack/Logic/ParcelCalculator.cs
+++ RapidPack/Logic/ParcelCalculator.cs
@@ -13,8 +13,42 @@ namespace RapidPack.Logic
     {
         public decimal CalculatePrice(double width, double height, double depth, double weight, bool isExpress, PackageType type)
         {
-            // logika w #6
-            return 0;
+            
+            if (weight > 30)
+            {
+                throw new ArgumentException("Błąd: Firma RapidPack nie obsługuje paczek cięższych niż 30 kg.");
+            }
+
+            
+            if (type == PackageType.Pallet)
+            {
+                return isExpress ? 115m : 100m; 
+            }
+
+            
+            decimal basePrice = 10m;
+            decimal weightPrice = (decimal)weight * 2m;
+            decimal currentTotal = basePrice + weightPrice;
+
+            
+            if (type == PackageType.Careful)
+            {
+                currentTotal += 10m;
+            }
+
+            
+            if ((width + height + depth) > 150)
+            {
+                currentTotal *= 1.5m; 
+            }
+
+            
+            if (isExpress)
+            {
+                currentTotal += 15m;
+            }
+
+            return currentTotal;
         }
     }
 }
